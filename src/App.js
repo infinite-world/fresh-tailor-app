5bf258e9587a9f80524fe5839fbe89f466b17a29
infinite-world <abdulrehhman711@gmail.com>
2021-01-12 15:21:07 +0500

    login-signup-changes-done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./components/loginForm/login";
+import Signup from "./components/signupFrom/signup";
 // components import
-import Signup from "./components/SignupForm/Signup";
-import Login from "./components/LoginForm/Login";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/signup" component={Signup} />
-                <Route path="/login" component={Login} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/loginForm/login.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
+import Navbar from "../Home/homeNavbar/homenavbar";
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <div class="container register">
+        <div class="row">
+          <div class="col-md-3 register-left">
+            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
+            <h3>Welcome</h3>
+            <p>TailorApp helps you to connect and manage your customers!</p>
+            <Link to="/signup">
+              <input type="submit" name="" value="signup" />
+            </Link>
+            <br />
+          </div>
+          <div class="col-md-9 register-right">
+            <div class="tab-content" id="myTabContent">
+              <div
+                class="tab-pane fade show active"
+                id="home"
+                role="tabpanel"
+                aria-labelledby="home-tab"
+              >
+                <h3 class="register-heading">Login To Your Account</h3>
+                <div class="row register-form">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <input
+                        type="email"
+                        class="form-control"
+                        placeholder=" Enter Email"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="password"
+                        name="txtEmpPhone"
+                        class="form-control"
+                        placeholder=" Enter Password"
+                      />
+                    </div>
+                    <div>
+                      <input type="checkbox" />
+                      <label className="pl-2">Keep me logged In</label>
+                    </div>
+
+                    <input type="submit" class="btnRegister" value="LogIn" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
